e30b9c78058c40e3f34a79034d0bf9dddbe63584
hornie <hornminer@gmail.com>
2018-12-10 02:11:15 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+var api = 'http://149.154.69.139:18622';
+var blockTargetInterval = 120.0;
+var coinUnits = 1000000000;
+var symbol = 'HLEB';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "HLEB": [
+        ["pool.hleb.cc", "https://pool.hleb.cc:8117"],	
+    ]
+};
